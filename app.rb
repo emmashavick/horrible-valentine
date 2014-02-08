3bf9b55e5cffa680522316a612804a453a23990c
Emma <emmashavick@gmail.com>
2014-02-08 19:27:00 +0000

    updated all if statements

--- app.rb
+++ app.rb
@@ -8,12 +8,12 @@ post '/' do
   # debug
   # raise params.inspect
 
-  significant_other = params[:significant_other_is]
+  significant_other = params[:me]
   likes  = params[:likes]
 
   # horrible nested if/else here
   # change me please!
-  if significant_other == 'sport' && significant_other == 'animals'
+  if me == 'animal' && (likes == 'outdoor' || likes = 'science' || likes = 'sports')
     @gift_name = 'Vet'
     @gift_url  = 'http://www.rcvs.org.uk/education/i-want-to-be-a-vet/'
     @gift_img  = 'vet.png'
@@ -23,42 +23,41 @@ post '/' do
       @gift_url  = 'http://www.padi.com/scuba/scuba-diving-guide/teach-scuba-diving/go-pro-scuba-diving-careers/'
       @gift_img  = 'scuba.jpeg'
     else
-      if likes == 'gardening'
-        @gift_name = 'The Humanure Handbook: A Guide to Composting Human Manure'
-        @gift_url  = 'http://www.amazon.com/The-Humanure-Handbook-Composting-Edition'
-        @gift_img  = 'The-Humanure-Handbook.jpg'
+      if me == 'geek' && (likes == 'science' || likes = 'outdoor')
+        @gift_name = 'Computer Scientist'
+        @gift_url  = 'http://www.city.ac.uk/courses/undergraduate/computer-science'
+        @gift_img  = 'geek.jpg'
       else
-        if likes == 'music'
-          if significant_other == 'male'
-            @gift_name = '50 Kazoos, Assorted Colors'
-            @gift_url  = 'http://www.amazon.com/Hohner-Inc-USA-KC-50/dp/B0002F4TJ6'
-            @gift_img  = '50-Kazoos.jpg'
-          else # significant_other == 'female'
-            @gift_name = 'I\'m Sick of You, by Iggy & The Stooges'
-            @gift_url  = 'http://www.amazon.com/Sick-You-Iggy-The-Stooges/dp/B000003JFN'
-            @gift_img  = 'Iggy-The-Stooges-Im-Sick-Of-You.jpg'
-          end
+        if (me == 'geek' && likes == 'sports') || (me == 'sports' && likes == science)
+          @gift_name = 'Sports Science Engineer'
+          @gift_url = 'https://www.strath.ac.uk/dmem/prospectivestudents/undergraduatecourses/sportsengineering/'
+          @gift_img = 'sport-science.jpg'
+
         else
-          if likes == 'animals'
-            if significant_other == 'male'
-              @gift_name = 'Sheep! Magazine'
-              @gift_url  = 'http://www.amazon.com/Sheep-Magazine/dp/B00006KWTM/'
-              @gift_img  = 'Sheep-Magazine.jpg'
-            else # significant_other == 'female'
-              @gift_name = 'Flattened Fauna, Revised: A Field Guide to Common Animals of Roads, Streets, and Highways'
-              @gift_url  = 'http://www.amazon.com/Flattened-Fauna-Revised-Animals-Highways'
-              @gift_img  = 'Flattened-Fauna.jpg'
-            end
-          else
-            if likes == 'sports'
-              if significant_other == 'male'
-                @gift_name = 'Anger Management For Dummies'
-                @gift_url  = 'http://www.amazon.com/Anger-Management-Dummies-Doyle-Gentry/'
-                @gift_img  = 'Anger-Management-For-Dummies.png'
-              else # significant_other == 'female'
-                @gift_name = 'Scale plus Body Fat Monitor Duo'
-                @gift_url  = 'http://www.amazon.com/Tanita-BF680W-Scale-Monitor-Athletic/dp/B0009V1YPU/'
-                @gift_img  = 'Scale-Plus-Body-Fat-Monitor.jpg'
+          if me == 'geek' && likes == 'music'
+        @gift_name = 'Sound Engineer'
+        @gift_url  = 'http://www.pointblanklondon.com/courses/music-production-courses.php'
+        @gift_img  = 'sound.jpg'
+
+        else
+          if me == 'sport' && (likes == 'sports' || likes = 'outdoor')
+        @gift_name = 'Professional Athlete'
+        @gift_url  = 'http://money.howstuffworks.com/how-to-become-a-professional-athlete.htm'
+        @gift_img  = 'athlete.jpg'
+
+        else
+          if me == 'sport' && likes == 'sports'
+        @gift_name = 'Professional Dancer'
+        @gift_url  = 'https://www.rad.org.uk/'
+        @gift_img  = 'dance.jpg'
+
+        else
+          if me == 'animal' && likes == 'music'
+        @gift_name = 'Animal-Choreography Stunt Artist'
+        @gift_url  = 'http://www.halfanimal.com/category/choreography-stunt-coordinating/'
+        @gift_img  = 'hippo.jpg'
+
+      
               end
             end
           end

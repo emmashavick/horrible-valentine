6c73edaf441c78717a7798d579d0d94c2f5fb83d
Emma <emmashavick@gmail.com>
2014-02-09 12:36:23 +0000

    updated 'if' statements

--- app.rb
+++ app.rb
@@ -19,42 +19,42 @@ post '/' do
       @gift_img  = 'scuba.jpeg'
 
     else
-      if me == 'animal' && (likes == 'outdoor' || likes == 'science' || likes == 'sports')
+      if me == 'animal' && (if likes == 'outdoor' || if likes == 'science' || if likes == 'sports')
     @gift_name = 'Vet'
     @gift_url  = 'http://www.rcvs.org.uk/education/i-want-to-be-a-vet/'
     @gift_img  = 'vet.png'
 
     else
-      if me == 'geek' && (likes == 'science' || likes == 'outdoor')
+      if me == 'geek' && (if likes == 'science' || if likes == 'outdoor')
         @gift_name = 'Computer Scientist'
         @gift_url  = 'http://www.city.ac.uk/courses/undergraduate/computer-science'
         @gift_img  = 'geek.jpg'
       else
-        if (me == 'geek' && likes == 'sports') || (me == 'sports' && likes == science)
+        (if me == 'geek' && if likes == 'sports') || (if me == 'sports' && if likes == science)
           @gift_name = 'Sports Science Engineer'
           @gift_url = 'https://www.strath.ac.uk/dmem/prospectivestudents/undergraduatecourses/sportsengineering/'
           @gift_img = 'sport-science.jpg'
 
         else
-          if me == 'geek' && likes == 'music'
+          if me == 'geek' && if likes == 'music'
         @gift_name = 'Sound Engineer'
         @gift_url  = 'http://www.pointblanklondon.com/courses/music-production-courses.php'
         @gift_img  = 'sound.jpg'
 
         else
-          if me == 'sport' && (likes == 'sports' || likes == 'outdoor')
+          if me == 'sport' && (if likes == 'sports' || if likes == 'outdoor')
         @gift_name = 'Professional Athlete'
         @gift_url  = 'http://money.howstuffworks.com/how-to-become-a-professional-athlete.htm'
         @gift_img  = 'athlete.jpg'
 
         else
-          if me == 'sport' && likes == 'sports'
+          if me == 'sport' && if likes == 'sports'
         @gift_name = 'Professional Dancer'
         @gift_url  = 'https://www.rad.org.uk/'
         @gift_img  = 'dance.jpg'
 
         else
-          if me == 'animal' && likes == 'music'
+          if me == 'animal' && if likes == 'music'
         @gift_name = 'Animal-Choreography Stunt Artist'
         @gift_url  = 'http://www.halfanimal.com/category/choreography-stunt-coordinating/'
         @gift_img  = 'hippo.jpg'
